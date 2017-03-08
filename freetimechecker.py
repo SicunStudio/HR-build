5231a3583e05603d374d5773d60c9754e30f1b4a
AnClark <clarklaw4701@qq.com>
2017-03-08 01:49:41 +0000

    Improved CSS and JS logic

--- freetimechecker.py
+++ freetimechecker.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -10,6 +10,12 @@ def hello_world():
 @app.route('/old')
 def page_old():
     return render_template('old_style_with_table.html')
+
+@app.route('/submit_freetime', methods=('GET', 'POST'))
+def submit_freetime():
+    raw_data = request.form.get('result','')
+    print(raw_data)
+    return jsonify(backMessage={'message':raw_data})
 
 if __name__ == '__main__':
     app.run(debug=True)
