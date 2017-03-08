2b01c6805511af9c9074d79f037da49ae7452b75
AnClark <clarklaw4701@qq.com>
2017-03-08 15:22:44 +0000

    Introduce Free Time manager #1

--- starter.py
+++ starter.py
@@ -316,6 +316,28 @@ def searching_score():
     return jsonify(result=grepScore(direction, content))
 
 
+###### TODO: TEST AREA! ######
+@app.route('/submit_freetime', methods=('GET', 'POST'))
+def submit_freetime():
+    raw_data = request.form.get('result','')
+    output=parse_result(raw_data=raw_data)
+
+    return jsonify(backMessage={'message':output})
+
+
+def parse_result(raw_data):
+    result=[]
+    weekdays_name = {
+        'MON': '周一', 'TUE': '周二', 'WED': '周三', 'THU': '周四', 'FRI': '周五', 'SAT': '周六', 'SUN': '周日'
+    }
+    lesson_time = raw_data.split(',')
+    for item in lesson_time:
+        buff=item.split('-', 1)
+        day=buff[0]
+        time=buff[1]
+        result.append("%s %s节" % (weekdays_name[day], time))
+
+    return result
 
 
 ######## run ########
